d4fbe04f31d68e4803bec92e5145b8e42fda73c7
Andy Barajas <andybarajas0418@gmail.com>
2020-02-02 10:12:00 -0500

    Add project files.

---
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// you will use the Sql Server provider, however it would also be permissible to use
+// ADO.NET
+// factory pattern for greater flexibilty
+using System.Data;
+using System.Data.SqlClient;
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.DataOperations
+{
+    public class InventoryDAL
+    {
+        private readonly string _connectionString;
+        private SqlConnection _sqlConnection = null;
+        
+
+        public InventoryDAL() : this(@"Data Source=masterdb.cc4nxjhwbbqs.us-east-2.rds.amazonaws.com,1433; Initial Catalog=AutoLot; User=admin; Password=507916ab;")
+        {
+
+        }
+
+        public InventoryDAL(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        private void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection { ConnectionString = _connectionString };
+            _sqlConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            if(_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+        }
+
+        public List<Car> GetAllInventory()
+        {
+            OpenConnection();
+            // this will hold the records
+            List<Car> inventory = new List<Car>();
+
+            // prep the command object
+            string sql = "SELECT * FROM Inventory;";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dataReader.Read())
+                {
+                    inventory.Add(new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    });
+                }
+                dataReader.Close();
+            }
+            return inventory;
+        }
+        public Car GetCar(int id)
+        {
+            OpenConnection();
+            Car car = null;
+            string sql = $"SELECT * FROM Inventory WHERE CarId = {id}";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    };
+                } dataReader.Close();
+            }
+            return car;
+        }
+    }
+}
---
+++ AutoLotDataReader/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Management.Instrumentation;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+namespace AutoLotDataReader
+{
+    class Program
+    {
+        public enum ConnectionState
+        {
+            Broken,
+            Closed,
+            Connecting,
+            Executing,
+            Fetching,
+            Open
+        }
+
+        public enum CommandType
+        {
+            StoredProcedure,
+            TableDirect,
+            Text // Default value.
+        }
+        static void Main(string[] args)
+        { 
+            WriteLine("**** Fun with Data Readers ****\n"); 
+            
+            //Create a connection string using the stringbuilder
+            var cnStringBuilder = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "AutoLot",
+                DataSource = "masterdb.cc4nxjhwbbqs.us-east-2.rds.amazonaws.com,1433",
+                ConnectTimeout = 30,
+                UserID = "admin",
+                Password = "507916ab"
+
+            };
+
+
+
+            // Create and open a connection.
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+
+                // Create a SQL command object
+                string sql = "SELECT * FROM Inventory; SELECT * FROM Customers";
+                SqlCommand myCommand = new SqlCommand(sql,connection);
+                // Create another command object via properties
+                SqlCommand testCommand = new SqlCommand();
+                testCommand.Connection = connection;
+                testCommand.CommandText = sql;
+
+                // obtain a data reader a la ExecuteReader()
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    // loop over the results
+                    while (myDataReader.Read())
+                    {
+                        WriteLine("*** Record ***");
+                        for (int i = 0;i < myDataReader.FieldCount; i++ )
+                        {
+                            WriteLine($"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)}");
+                        }
+
+                        WriteLine();
+                    }
+
+                    while (myDataReader.NextResult()) ;
+                }
+            }
+            ReadLine();
+        }
+
+        public static void ShowConnectionStatus(SqlConnection connection)
+        {
+            // show various stats about the current connection object
+            WriteLine("**** Info about your connection *****");
+            WriteLine($"Database location: {connection.DataSource}");
+            WriteLine($"Database name: {connection.Database}");
+            WriteLine($"Timeout: {connection.ConnectionTimeout}");
+            WriteLine($"Connection state: {connection.State}\n");
+        }
+    }
+}
